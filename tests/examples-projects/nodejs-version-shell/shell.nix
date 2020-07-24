9c88c6ff6752c382b2b0c99010b6d058cb146c07
Tobias Pflug <tobias.pflug@gmail.com>
2020-07-24 16:40:29 +0200

    Add comment explaining nodejs version assert

--- tests/examples-projects/nodejs-version-shell/shell.nix
+++ tests/examples-projects/nodejs-version-shell/shell.nix
@@ -2,7 +2,10 @@
 let
   node = pkgs.nodejs-10_x;
 in
-assert pkgs.nodejs == node -> throw "default nodejs version has been updated. Please update the test";
+# We need make sure that `nodejs` does not default to `nodejs-10_x` because
+# then our test cannot ensure that we can override the default. If the assert
+# below throws, change `node` above to a different version.
+assert pkgs.nodejs == node -> throw "`nodejs` is refering to `nodejs-10_x` rendering this test ineffective.";
 pkgs.npmlock2nix.shell {
   src = ./.;
   nodejs = node;
