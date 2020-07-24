2005c2acd91d26a2f5209497a621143e9cb9a553
Tobias Pflug <tobias.pflug@gmail.com>
2020-07-24 12:13:13 +0200

    Add smoke to niv deps

--- nix/default.nix
+++ nix/default.nix
@@ -5,6 +5,7 @@ in import sources.nixpkgs {
   overlays = [
     (self: super: {
       npmlock2nix = self.callPackage ../default.nix {};
+      inherit (self.callPackage (import sources.smoke) {}) smoke;
     })
   ];
 }
