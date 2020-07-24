fa782b9f75e1296c19031d11153831407334a46b
Andreas Rammhold <andreas@rammhold.de>
2020-07-24 17:31:14 +0200

    Add niv to shell since we use that for dependency management

--- tests/integration-tests/default.nix
+++ tests/integration-tests/default.nix
@@ -1,5 +1,19 @@
-{ npmlock2nix, testLib }:
+{ npmlock2nix, testLib, callPackage }:
 testLib.makeIntegrationTests {
+
+  shell = {
+    description = "Require this projects shell expression to work";
+    shell = import ../../shell.nix;
+    command = ''
+      type -a test-runner > /dev/null
+      type -a node > /dev/null
+      type -a smoke > /dev/null
+      type -a niv > /dev/null
+      type -a nixpkgs-fmt > /dev/null
+    '';
+    expected = "";
+  };
+
   leftpad = {
     description = "Require a node dependency inside the shell environment";
     shell = npmlock2nix.shell { src = ../examples-projects/single-dependency; };
