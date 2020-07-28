0e8d932a9efa425e7d05210bc85e8c0758f409c6
Andreas Rammhold <andreas@rammhold.de>
2020-07-28 10:49:23 +0200

    run integration-tests in temporary working directory by default

--- tests/lib.nix
+++ tests/lib.nix
@@ -20,18 +20,29 @@
     if builtins.length failures == 0 then [ ] else
     builtins.throw msg;
 
-  # Takes an attribute set of tests { description, shell, command, expected, (optional) status }
+  # Takes an attribute set of tests { description, shell, command, expected, (optional) status, (optional) temporary-directory = true }
   # an creates a bats script that executes them.
   makeIntegrationTests = tests:
     let
-      mkTestScript = name: shell: command:
+      mkTestScript = name: test:
         let
-          shellDrv = (shell.overrideAttrs (_: { phases = [ "noopPhase" ]; noopPhase = "touch $out"; })).drvPath; in
+          shellDrv = (test.shell.overrideAttrs (_: { phases = [ "noopPhase" ]; noopPhase = "touch $out"; })).drvPath;
+          temporaryDirectory = test.temporary-directory or true;
+        in
         writeShellScript name ''
           export PATH="${nix}/bin:${coreutils}/bin"
-          exec nix-shell --pure ${shellDrv} --run "${writeShellScript "${name}-command" command}"
+          set -e
+          ${lib.optionalString temporaryDirectory ''
+            WORKING_DIR=$(mktemp -d)
+            function cleanup {
+              rm -rf "$WORKING_DIR"
+            }
+            trap cleanup EXIT
+            cd $WORKING_DIR
+          ''}
+          nix-shell --pure ${shellDrv} --run "${writeShellScript "${name}-command" test.command}"
         '';
-      testScripts = lib.mapAttrs (name: test: test // { script = mkTestScript name test.shell test.command; inherit name; }) tests;
+      testScripts = lib.mapAttrs (name: test: test // { script = mkTestScript name test; inherit name; }) tests;
 
       smokeConfig.tests = map
         (test: {
