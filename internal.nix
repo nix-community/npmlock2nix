812246cf6a61d3be1a6a79481d7d71c59802c44a
kghugo <hugosum.dev@protonmail.com>
2021-10-04 15:14:03 +0100

    Double quote to prevent globbing and word-splitting

--- internal.nix
+++ internal.nix
@@ -379,7 +379,7 @@ rec {
           runHook postBuild
         '';
         installPhase = ''
-          mkdir $out
+          mkdir -p "$out"
 
           if test -d node_modules; then
             if [ $(ls -1 node_modules | wc -l) -gt 0 ] || [ -e node_modules/.bin ]; then
