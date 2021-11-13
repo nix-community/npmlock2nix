673435463ed72dc6d7c3f316dbf53d68787974d7
Andreas Rammhold <andreas@rammhold.de>
2021-11-13 21:25:02 +0100

    Make our nix/default.nix file compatible with Nix 2.4+
    Nix 2.4 introduced a breakage/bug that requies anything that pretends to
    be nixpkgs to accept arbitrary arguments. We just accept and ignore them
    for now.

--- nix/default.nix
+++ nix/default.nix
@@ -1,4 +1,8 @@
-{}:
+/*
+  We have to ignore additional parameters passed to us as Nix 2.4 is injecting `inNixShell` without checking if the called expression actually wants it
+  See: https://github.com/NixOS/nix/pull/5543
+*/
+{ ... }:
 let
   sources = import ./sources.nix;
 in
