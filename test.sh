723bd057be743281b676da095f1dd4ed9813ee25
Martin Kühl <martin.kuehl@posteo.net>
2022-02-28 17:41:13 +0100

    test: remove unnecessary command substitution

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ echo -e "Running unit tests\n"
 nix-build -A tests --no-build-output --no-out-link --show-trace
 
 echo -e "\n\nRunning integration tests\n"
-$(nix-build  -A tests.integration-tests --no-out-link --show-trace)
+nix-build  -A tests.integration-tests --no-out-link --show-trace
 
 echo -e "\n\nTest githubSourceHashMap in restricted mode\n"
 nix-build tests/examples-projects/github-dependency/default.nix --restrict-eval -I . --allowed-uris 'https://github.com/nixos/nixpkgs' --show-trace
